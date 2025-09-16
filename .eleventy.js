fadf60cc20596b1f7dfc21f7ec909cedc16921d7
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2025-09-16 17:00:47 +0200

    Added prod and dev and base layout for index

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,26 @@
-module.exports = function(eleventyConfig){
+module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("src/static/style.css")
-    eleventyConfig.addPassthroughCopy("src/static/script.js")
-    eleventyConfig.addPassthroughCopy("src/static/images")
-    eleventyConfig.addPassthroughCopy("src/static/svg")
-    return {
-        dir: {
-          input: "src",
-          output: "docs"
-        }
-      };
-}
+  // Passthroughs
+  eleventyConfig.addPassthroughCopy("src/static/style.css");
+  eleventyConfig.addPassthroughCopy("src/static/script.js");
+  eleventyConfig.addPassthroughCopy("src/static/images");
+  eleventyConfig.addPassthroughCopy("src/static/svg");
+
+  // Global Data
+  eleventyConfig.addGlobalData("global", () => {
+    if (process.env.ELEVENTY_ENV === "prod") {
+      return { url: "https://webcommits.github.io/gpupgrade" };
+    } else {
+      return { url: "" };
+    }
+  });
+
+  // Input/Output
+  return {
+    dir: {
+      input: "src",
+      output: "docs",
+      includes: "_includes"
+    }
+  };
+};
